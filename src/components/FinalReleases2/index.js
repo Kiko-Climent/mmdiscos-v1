03fa2f9c767ea32c03e2c36135a63e82648c6721
Kiko Climent <kikocliment@Mac.fritz.box>
2026-05-10 23:48:52 +0200

    Change Index view on mobile new approach?

--- src/components/FinalReleases2/index.js
+++ src/components/FinalReleases2/index.js
@@ -6,6 +6,7 @@ import { InfoPanel, TrackPanel, MobilePanel } from "./panels";
 import IndexView from "./IndexView";
 import { PADDING_PX, INFO_W_FRAC } from "./constants";
 import { CENTER_IDX, SLIDE_COUNT, TITLES } from "./releaseMap";
+import IndexView2 from "./IndexView2";
 
 export default function FinalReleases2() {
   const canvasRef     = useRef(null);
@@ -128,7 +129,7 @@ export default function FinalReleases2() {
           transition: "opacity 0.45s ease",
         }}
       >
-        <IndexView />
+        <IndexView2 />
       </div>
 
       {/* Grain */}
