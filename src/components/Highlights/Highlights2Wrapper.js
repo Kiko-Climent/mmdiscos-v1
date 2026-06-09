fd3acbd6b255533aa9eeeebde74d5d4508918012
Kiko Climent <kikocliment@MacBook-Air-de-Kiko.fritz.box>
2026-06-10 00:34:20 +0200

    fine tunning, back to previous version of  highlights mobile

--- src/components/Highlights/Highlights2Wrapper.js
+++ src/components/Highlights/Highlights2Wrapper.js
@@ -21,7 +21,7 @@ export default function Highlights2Wrapper() {
         <Highlights3_3Desktop_2 />
       </div>
       <div className="block min-[901px]:hidden">
-        <Highlights3_3Mobile3 />
+        <Highlights3_3Mobile_2 />
       </div>
     </>
   );
